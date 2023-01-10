75898c569d1d4eab970d035eeb38a759514d7c34
kkndesatalang133 <122349917+kkndesatalang133@users.noreply.github.com>
2023-01-10 15:35:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// alert("Selamat Data di Web Generasi Anak Muda");
+
+// var lagi = true;
+
+// while (lagi === true) {
+//     var Nama = prompt("Masukkan Nama : ");
+//     alert("Hallo " + Nama);
+//     alert("Selamat Datang " + Nama);
+//     lagi = confirm("Apakah Ingin memasukkan data kembali ???");
+// }
+// alert("Terimakasih ...");
+
+// console.log("Hello World");
+
+// var nama1 = prompt("Masukkan Nama Kmu untuk Melanjutkan");
+// if (nama1 === "Hariando") {
+//     alert("Miranda");
+// }
